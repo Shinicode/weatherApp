83d46351cdd067516204faaa0da2980f5bb38569
shinicode <nakinakou@gmail.com>
2019-07-11 16:55:59 +0300

    Add about text

--- src/app.js
+++ src/app.js
@@ -73,11 +73,6 @@ app.get('/weather', (req,res) => {
 
         })
     })
-    // res.send({
-    //     forecast: 'It is snowing',
-    //     location : 'Philadelphia',
-    //     address : req.query.address
-    // })
 })
 
 
